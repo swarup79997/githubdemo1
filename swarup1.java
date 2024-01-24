491359fbe45869f082678123b5b2d073f07bcd1e
chennuru swarup kumar reddy <137624594+swarup79997@users.noreply.github.com>
2024-01-24 11:40:27 +0530

    Update swarup1.java

--- swarup1.java
+++ swarup1.java
@@ -2,5 +2,6 @@ public class swarup1 {
     public static void main(String[] args) {
         System.out.println("Hello swarup");
         System.out.println("Hello Mahesh");
+        System.out.println("Hello Mahi");
     }
 }
